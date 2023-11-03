9f461ab15c7fd87b9e6e70129d03b08a10d5357e
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-11-03 14:04:12 -0300

    Create next app + borrow strategy from nextjs-use-php

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24 bg-white">
+      <form>
+        <button
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          formAction={async () => {
+
+          }}>
+          Say hello
+        </button>
+      </form>
+    </main>
+  )
+}
